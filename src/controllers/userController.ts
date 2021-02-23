84d6a50dd23f7dc3f44cf98288e00e1d0ddf8ff8
Giovanny Lucas Alves de Oliveira <giovannylucax@gmail.com>
2021-02-23 11:59:49 -0300

    feat: :sparkles: adicionada primeira entidade e primeiro insert de usuários

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,27 @@
 import { Request, Response } from "express";
 
+import { User } from "../models/User";
+import { getRepository } from "typeorm";
+
 export class UserController {
   async create (request: Request, response: Response) {
-    return response.send('chegou aqui')
+    const { name, email } = request.body;
+
+    const usersRepository = getRepository(User);
+
+    const userAlreadyExists = await usersRepository.findOne({ email })
+
+    if (userAlreadyExists) {
+      return response.status(409).json({ error: 'User already exists!' })
+    }
+
+    const user = await usersRepository.save(
+      usersRepository.create({
+        name,
+        email
+      })
+    )
+
+    return response.send(user)
   }
 }
